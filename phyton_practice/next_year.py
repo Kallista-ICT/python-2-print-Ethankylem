b91491b5f47ccef3afe3d972f57ca4071b1d9256
Ethankylem <ethanmarciano065@gmail.com>
2025-02-12 00:45:47 +0000

    submit

---
+++ phyton_practice/next_year.py
@@ -0,0 +1,8 @@
+print("Hello, World!")
+name=input("Enter your name:")#command
+print(f"Hello {name}")
+age=int(input("Enter your age:"))
+print(f"hello {name} next year your age will be {age + 1}")
+
+
+
